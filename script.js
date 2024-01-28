8f1a89005b8cd133d37c5b338a4b699de254a60a
katyaandrushko <katyaandrushko14@gmail.com>
2024-01-28 20:58:00 +0200

    Update

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let result = document.getElementById('result')
 let getMovie = () => {
    let movieName = movieNameRef.value
    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`
+
    // input field = empty
    if (movieName.length <= 0) {
       result.innerHTML = `<h3 class='msg'>Please Enter a Movie Name</h3>`
@@ -16,15 +17,40 @@ let getMovie = () => {
       fetch(url)
          .then((resp) => resp.json())
          .then((data) => {
-            console.log(data)
-            console.log(data.Poster)
-
-            result.innerHTML = `
+            if (data.Response == 'True') {
+               result.innerHTML = `
       <div class='info'>
          <img src=${data.Poster} class='poster'> </img>
-      </div>`
+         <div>
+            <h2> ${data.Title}</h2>
+            <div class='rating'>
+                     <img src='star-icon.svg'> 
+                     <h4> ${data.imdbRating} </h4>
+               </div>
+                  <div class='details'> 
+                     <span>${data.Rated}</span>
+                     <span>${data.Year}</span>
+                     <span>${data.Runtime}</span>
+                  </div>
+                  <div class='genre'>
+                     <div>${data.Genre.split(',').join('</div><div>')}</div>
+                  </div>
+            </div>
+      </div>
+
+         <h3>About:</h3>
+           <p>${data.Plot}</p>
+             <h3>Actors:</h3>
+           <p>${data.Actors}</p>`
+            } else {
+               result.innerHTML = `<h3 class='msg'>${data.Error}</h3>`
+            }
+         })
+         .catch(() => {
+            result.innerHTML = `<h3 class='msg'>Error</h3>`
          })
    }
 }
 
+searchBtn.addEventListener('click', getMovie)
 window.addEventListener('load', getMovie)
